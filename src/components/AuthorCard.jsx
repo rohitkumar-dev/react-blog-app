7b8a3c963eba6dba558da8b4c7aa984bcb46764b
Rohit Kumar <contactme.rohitkumar@gmail.com>
2025-09-15 12:54:39 +0530

    updared

--- src/components/AuthorCard.jsx
+++ src/components/AuthorCard.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 function AuthorCard() {
     const authorData = [
         {
-            name: "Minuises",
-            profile: "https://images.pexels.com/photos/680239/pexels-photo-680239.jpeg",
+            name: "Nobiaseisa",
+            profile: "https://images.unsplash.com/photo-1484186139897-d5fc6b908812?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             type: "World Traveller",
         },
-        {
+         {
             name: "Louisana",
-            profile: "https://images.pexels.com/photos/160959/girl-portrait-dark-eyes-smile-160959.jpeg",
-            type: "Politician",
+            profile: "https://images.unsplash.com/photo-1755157161839-0b0fbd5fef74?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            type: "Nature Lover",
         },
         {
-            name: "Nobiaseisa",
-            profile: "https://images.pexels.com/photos/1066137/pexels-photo-1066137.jpeg",
-            type: "Social Activist",
+            name: "Georgesilia",
+            profile: "https://images.unsplash.com/photo-1543123820-ac4a5f77da38?q=80&w=836&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            type: "Mountain Explorer",
         },
+        
 
     ]
   return (
@@ -24,7 +25,7 @@ function AuthorCard() {
         { authorData && authorData.map((author,i)=>(
             <div key={i} className=' bg-red-300 mx-6 sm:mx-24 my-12 sm:my-15 pt-10 pb-5 rounded-lg shadow-lg'>
             <div className=' flex flex-col w-full items-center text-red-900'>
-                <img src={author.profile} alt="profile" className='h-50 w-50  rounded-full object-cover outline-8 outline-offset-8 outline-white border-2 border-white'/>
+                <img src={author.profile} alt="profile" className='h-50 w-50  rounded-full object-cover outline-6 outline-offset-6 outline-red-900 border-2 border-red-600'/>
                 <h2 className='pt-5 text-xl font-medium '>{author.name}</h2>
                 <p className='pt-1 text-sm font-medium '>{author.type}</p>
             </div>
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 function ImageGallery() {
   const images = [
-    "https://images.pexels.com/photos/1140867/pexels-photo-1140867.jpeg",
-    "https://images.pexels.com/photos/1140854/pexels-photo-1140854.jpeg",
-    "https://images.pexels.com/photos/1141436/pexels-photo-1141436.jpeg",
+    "https://images.unsplash.com/photo-1651457897431-ee5584e0d4ba?q=80&w=1205&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.pexels.com/photos/8156192/pexels-photo-8156192.jpeg",
+    "https://images.unsplash.com/photo-1705769942096-7306c36742fe?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   ];
 
   return (
       <div className="h-full mx-6 pt-2 pb-12 sm:mx-24 ">
         {images.map((image, i) => (
-          <div key={i} className="h-60 pt-0">
+          <div key={i} className="h-60 pt-0 rounded-lg ">
             <img
               src={image}
               alt="gallery image"
-              className="w-full h-full object-cover mt-10 rounded-lg "
+              className="w-full h-60 object-cover mt-10 rounded-lg "
             />
             
           </div>
